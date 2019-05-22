ca88cce60108c292621c476497b566c0894a23cf
sophia wu <sophia6734wu@gmail.com>
2019-05-22 16:50:46 +1000

    simple airbnb

---
+++ app/models/flat.rb
@@ -0,0 +1,6 @@
+class Flat < ApplicationRecord
+  validates :name, :address, :price_per_night, :number_of_guests, presence: true
+  validates :price_per_night, :numericality => { :only_integer => true }
+  validates :number_of_guests, :numericality => { :only_integer => true }
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,36 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 68,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Stylish House Close to River Thames',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 70,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat Taiwan',
+  address: '10 Quees Rd Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 50,
+  number_of_guests: 2
+)
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
